05a6e6b6f8bd7eaab78be78ede0a22745dbb200b
lynng-dev <lynn.germiquet@gmail.com>
2025-11-06 18:07:34 +0100

    Assignment update

--- Lessons/Lesson-04/Functions/script.js
+++ Lessons/Lesson-04/Functions/script.js
@@ -83,8 +83,27 @@
 // }
 
 // five - for
-let number = 0;
+// let number = 0;
 
-for(let number = 0; number < 1000; number++) {
-    console.log(number);
+// for(let number = 0; number < 10; number++) {
+//     console.log(number);
+// }
+
+
+//six
+const cats = ["Leopard", "Jaguar", "Tiger", "Lion"];
+
+for(let id = 0; id < cats.length; id++){
+    console.log(cats[id]);
 }
+
+
+document.addEventListener('keydown', (keyEvent) =>{
+    console.log(keyEvent.key);
+    if(keyEvent.key == ' '){
+        console.log("SPACE");
+    }
+    else if (keyEvent.key == '1'){
+        console.log("ONE")
+    }
+})
